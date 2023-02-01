75c3729df1b2974835940ce2f61c66d7e83a9c69
Marcus Sampaio <sampaiosoftware@gmail.com>
2023-02-01 16:19:08 -0300

    custom controls estilizados

--- js/modules/slide.js
+++ js/modules/slide.js
@@ -151,7 +151,7 @@ export class Slide {
   }
 }
 
-export class SlideNav extends Slide {
+export default class SlideNav extends Slide {
   constructor(...args) {
     super(...args);
     this.bindControlEvents();
