6eab12a60c33ef4bc5b7b8f71d8598842e4c48b4
Marcus Sampaio <sampaiosoftware@gmail.com>
2023-02-01 15:18:32 -0300

    adicionada paginacao por slide

--- js/modules/slide.js
+++ js/modules/slide.js
@@ -6,6 +6,7 @@ export class Slide {
     this.wrapper = document.querySelector(wrapper);
     this.dist = { finalPosition: 0, startX: 0, movement: 0 };
     this.activeClass = "active";
+    this.changeEvent = new Event("changeEvent");
   }
 
   transition(active) {
@@ -109,6 +110,7 @@ export class Slide {
     this.slidesIndexNav(index);
     this.dist.finalPosition = activeSlide.position;
     this.changeActiveClass();
+    this.wrapper.dispatchEvent(this.changeEvent);
     return this;
   }
 
@@ -150,14 +152,59 @@ export class Slide {
 }
 
 export class SlideNav extends Slide {
+  constructor(...args) {
+    super(...args);
+    this.bindControlEvents();
+  }
+
   addArrow(prev, next) {
     this.prevElement = document.querySelector(prev);
     this.nextElement = document.querySelector(next);
     this.addArrowEvent();
+    return this;
   }
 
   addArrowEvent() {
     this.prevElement.addEventListener("click", this.activePrevSlide);
     this.nextElement.addEventListener("click", this.activeNextSlide);
+  }
+
+  createControl() {
+    const control = document.createElement("ul");
+    control.dataset.control = "slide";
+    this.slideArray.forEach((item, index) => {
+      control.innerHTML += `<li><a href="#slide${index + 1}">${
+        index + 1
+      }</a></li>`;
+    });
+    this.wrapper.appendChild(control);
+    return control;
+  }
+
+  eventControl(item, index) {
+    item.addEventListener("click", (e) => {
+      e.preventDefault();
+      this.changeSlide(index);
+    });
+    this.wrapper.addEventListener("changeEvent", this.activeControleItem);
+  }
+
+  activeControleItem() {
+    this.controlArray.forEach((i) => i.classList.remove(this.activeClass));
+    this.controlArray[this.index.active].classList.add(this.activeClass);
+  }
+
+  addControl(customControl) {
+    this.control =
+      document.querySelector(customControl) || this.createControl();
+    this.controlArray = [...this.control.children];
+    this.activeControleItem();
+    this.controlArray.forEach(this.eventControl);
+    return this;
+  }
+
+  bindControlEvents() {
+    this.eventControl = this.eventControl.bind(this);
+    this.activeControleItem = this.activeControleItem.bind(this);
   }
 }
--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 import { SlideNav } from "./modules/slide.js";
 
 const slide = new SlideNav(".slide", ".wrapper");
-slide.init().addArrow(".prev", ".next");
+slide.init().addArrow(".prev", ".next").addControl();
